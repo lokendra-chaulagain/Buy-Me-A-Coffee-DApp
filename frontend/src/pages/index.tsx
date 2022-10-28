34f24a627a9b7904c83c9665532d725b7ef085b3
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2022-10-28 10:12:21 +0545

    integration successful

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,133 @@
-import React from "react";
-import { FaBeer } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import abi from "../utils/BuyMeACoffee.json";
+import { ethers } from "ethers";
 
 function Index() {
-  return <div>Index</div>;
+  const contractAddress = "0x8bd41849a3be6408Df57D801e744DF4244c5f79B";
+  const contractABI = abi.abi;
+
+  // Component state
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [memos, setMemos] = useState([]);
+
+  const onNameChange = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const onMessageChange = (event: any) => {
+    setMessage(event.target.value);
+  };
+
+  console.log(name);
+  console.log(message);
+
+  // Wallet connection logic
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      console.log("accounts: ", accounts);
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        console.log("wallet is connected! " + account);
+      } else {
+        console.log("make sure MetaMask is connected");
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("please install MetaMask");
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const buyCoffee = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+
+        // contract instance
+        const buyMeACoffee = new ethers.Contract(contractAddress, contractABI, signer);
+        console.log("buying coffee..");
+        const coffeeTxn = await buyMeACoffee.buyCoffee(name ? name : "anon", message ? message : "Enjoy your coffee!", { value: ethers.utils.parseEther("0.001") });
+        await coffeeTxn.wait();
+        console.log("mined ", coffeeTxn.hash);
+        console.log("coffee purchased!");
+        // Clear the form fields.
+        setName("");
+        setMessage("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Function to fetch all memos stored on-chain.
+  const getMemos = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const buyMeACoffee = new ethers.Contract(contractAddress, contractABI, signer);
+
+        console.log("fetching memos from the blockchain..");
+        const memos = await buyMeACoffee.getMemos();
+        console.log("fetched!");
+        setMemos(memos);
+      } else {
+        console.log("Metamask is not connected");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(memos)
+  // useEffect(() => {
+  //   let buyMeACoffee;
+  //   isWalletConnected();
+  //   getMemos();
+
+  //   // Create an event handler function for when someone sends
+  //   // us a new memo.
+  //   const onNewMemo = (from, timestamp, name, message) => {
+  //     console.log("Memo received: ", from, timestamp, name, message);
+  //     setMemos((prevState) => [
+  //       ...prevState,
+  //       {
+  //         address: from,
+  //         timestamp: new Date(timestamp * 1000),
+  //         message,
+  //         name
+  //       }
+  //     ]);
+  //   };
+
+  return (
+    <div className="">
+      <button onClick={connectWallet}>connect</button>
+      <input onChange={onNameChange} name="name" type="text" placeholder="name" />
+      <input onChange={onMessageChange} name="message" type="text" placeholder="message" />
+      <button onClick={buyCoffee}>buy coffee</button>
+    </div>
+  );
 }
 
 export default Index;
